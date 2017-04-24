1400b7ee8c827936d0520ba3120e519f4b669ef3
Edward Kim <ed@slickage.com>
2017-04-24 08:40:45 -1000

    Initial

---
+++ inMemDb.js
@@ -0,0 +1,55 @@
+const users = {
+  '1': { name: 'Alice', age: 12, gender: 'f', },
+  '2': { name: 'Bob', age: 21, gender: 'm', },
+  '3': { name: 'Claire', age: 56, gender: 'f', },
+  '4': { name: 'David', age: 9, gender: 'm', },
+  '5': { name: 'Eric', age: 61, gender: 'm', },
+  '6': { name: 'Fred', age: 45, gender: 'm', },
+  '7': { name: 'George', age: 38, gender: 'm', },
+  '8': { name: 'Hannah', age: 15, gender: 'f', },
+  '9': { name: 'Ilona', age: 17, gender: 'f', },
+  '10': { name: 'Jake', age: 27, gender: 'm', },
+  '11': { name: 'Kathy', age: 8, gender: 'f', },
+  '12': { name: 'Liam', age: 20, gender: 'm', }
+};
+
+const employer = {
+  dv: 'DevLeague',
+  st: 'Student',
+  na: 'N/A'
+};
+
+function byId() {
+
+}
+
+function youngest() {
+
+}
+
+function oldest() {
+
+}
+
+function males () {
+
+}
+
+function females() {
+
+}
+
+function jobs() {
+
+}
+
+const functions = {
+  byId: byId,
+  youngest: youngest,
+  oldest: oldest,
+  males: males,
+  females: females,
+  jobs: jobs
+};
+
+module.exports = functions;
